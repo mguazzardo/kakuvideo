b7432fbc100360ae5903b7f5078fd9956d498de4
mguazzardo <mguazzardo76@gmail.com>
2021-10-14 21:09:41 -0300

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://babackend:8080/api/topics";
+const BASE_URL = "http://backend:8080/api/topics";
 
 const API = axios.create({
   baseURL: BASE_URL,
